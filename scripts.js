9ffe80c12bafc06a90604fbbb96212de060f8af8
Paulo Kinopk <pkinopk@gmail.com>
2017-04-15 23:54:35 -0700

    Added Symmetric Difference

--- scripts.js
+++ scripts.js
@@ -83,7 +83,6 @@ function telephoneCheck(str) {
 //------------------------------------------------------------------------------
 //Record Collection
 
-
 // Setup
 var collection = {
   "2548": {
@@ -132,3 +131,6 @@ function updateRecords(id, prop, value) {
 // Alter values below to test your code
 //updateRecords(5439, "artist", "ABBA");
 //updateRecords(2468, "tracks", "Free");
+
+//------------------------------------------------------------------------------
+//Symmetric Difference
